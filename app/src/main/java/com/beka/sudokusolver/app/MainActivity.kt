7f2024c3f0cdd973ce2b2df05399a93d204108ac
Sharifov Sardorbek <sardorbek-sharifov@mail.ru>
2021-07-19 18:45:26 +0500

    include android

---
+++ app/src/main/java/com/beka/sudokusolver/app/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.beka.sudokusolver.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.solve_puzzle_button).setOnClickListener {
+            Toast.makeText(
+                this, "Button pressed", Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+}
